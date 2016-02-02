58c8924153a75d52c7fbff6268f53484e835ddbf
tbennett12 <tjb@iastate.edu>
2016-02-01 18:06:54 -0600

    Initial
    Initial commit of app.

---
+++ NutrientDataCollectionApp/app/src/main/java/com/ndca/nutrientdatacollectionapp/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.ndca.nutrientdatacollectionapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.NDCA.nutrientdatacollectionapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+
+
+    /** Called when the user clicks the History button */
+    public void goToHistory(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the New Sample button */
+    public void goToNewSample(View view) {
+        Intent intent = new Intent(this, NewSampleActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the New Sample button */
+    public void goToTutorial(View view) {
+        Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+    }
+}
